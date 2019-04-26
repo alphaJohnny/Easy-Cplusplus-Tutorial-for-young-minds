d28e882e4b44df9d9ee2a428242757e664c816b6
Alpha Johnny <johnny@malhotra.ca>
2019-04-26 12:03:57 -0400

    03.cpp

---
+++ 03.cpp
@@ -0,0 +1,45 @@
+/**
+ * In this section, we will explore the inbuilt types of Nouns or what we call variables in a program
+
+ * Here is the complete list of fundamental types in C++:
+ * Group	Type
+ * names,	Notes on size / precision *
+
+ * Character types
+ * char	Exactly one byte in size. At least 8 bits.
+ * char16_t	Not smaller than char. At least 16 bits.
+ * char32_t	Not smaller than char16_t. At least 32 bits.
+ * wchar_t	Can represent the largest supported character set.
+
+ * Integer types (signed)
+ * signed char	Same size as char. At least 8 bits.
+ * signed short int	Not smaller than char. At least 16 bits.
+ * signed int	Not smaller than short. At least 16 bits.
+ * signed long int	Not smaller than int. At least 32 bits.
+ * signed long long int	Not smaller than long. At least 64 bits.
+
+ * Integer types (unsigned)
+ * unsigned char	(same size as their signed counterparts)
+ * unsigned short int
+ * unsigned int
+ * unsigned long int
+ * unsigned long long int
+
+ * Floating-point types
+ * float
+ * double	Precision not less than float
+ * long double	Precision not less than double
+
+ * Boolean type
+ * bool
+ * Void type
+ * void	no storage
+ * Null pointer	decltype(nullptr)
+ */
+
+// Now let's use them in our code
+int int main(int argc, char const *argv[]) {
+  char my-first-character;
+  
+  return 0;
+}
